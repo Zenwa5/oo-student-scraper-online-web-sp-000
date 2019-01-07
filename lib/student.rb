93be50a876137d5922a39f4027c1f12068ae6727
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-01-07 00:28:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -25,5 +25,3 @@ class Student
       @@all
     end
   end
-
-  end 
