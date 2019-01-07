b80312af8280f5d47b84494c604101a5ee473efb
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-01-07 00:06:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+
+    #for the student name = doc.css(".student-name").text
+    #location = doc.css(".student-location").text
+    #website = student.css("a").attribute("href").value
+
+    
     html = open("./fixtures/student-site/index.html")
     doc = Nokogiri::HTML(html)
     doc.css(".student-card").map do |students|
@@ -12,12 +18,8 @@ class Scraper
         location: students.css(".student-location").text
         profile_url: students.css(".student-location").text
       }
-    end
-  end
-end
-    #for the student name = doc.css(".student-name").text
-    #location = doc.css(".student-location").text
-    #website = student.css("a").attribute("href").value
+    
+
   end
 
   def self.scrape_profile_page(profile_url)
